111b3451650ba4d310c0c3525bcb0ebb429223f5
Roberth Vestbø <74360236+roberthvestbo@users.noreply.github.com>
2022-01-11 21:28:27 +0100

    Updates

---
+++ precache-manifest.db4afafa79ff92df8ea767e7d199182c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "b721872ba64ce66e257c660e88daa17c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "af0d9e6b8f0a0467e86c",
+    "url": "/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "af0d9e6b8f0a0467e86c",
+    "url": "/static/js/main.af0d9e6b.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
