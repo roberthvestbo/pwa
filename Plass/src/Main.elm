71ac71b24d8f5901ced0012b1848b2733df30488
Roberth Vestbø <74360236+roberthvestbo@users.noreply.github.com>
2022-01-17 10:56:19 +0100

    Tweaks for publishing

--- Plass/src/Main.elm
+++ Plass/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, button, text, div, h1, img)
 import Html.Attributes exposing (src)
 
 
@@ -37,8 +37,9 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "../logo.svg" ] []
-        , h1 [] [ text "Your Elm App is almost working!" ]
+        [ img [ src "logo.svg" ] []
+        , h1 [] [ text "Your Elm App is working!" ]
+        , button [] [ text "I agree!"]
         ]
 
 
